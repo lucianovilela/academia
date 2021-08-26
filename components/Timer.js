d9bef37babb5740977f3b1cfc43a544626a77f97
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2021-08-26 15:32:21 -0300

    WIP

--- components/Timer.js
+++ components/Timer.js
@@ -1,99 +1,155 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
 import { View, ScrollView } from "react-native";
-import { Input, Button, Divider, ListItem, Text } from "react-native-elements";
+import { Button, Divider, ListItem, Text, Avatar } from "react-native-elements";
 import { FlatList } from "react-native-gesture-handler";
 import styles from './AppStyle';
-import ModalAtividade from "./ModalAtividade";
+
+//function to format the time hh:mm:ss
+function formatTime(time) {
+    let h = Math.floor(time / 3600);
+    let m = Math.floor((time - h * 3600) / 60);
+    let s = time - h * 3600 - m * 60;
+    return (h > 0 ? h + ":" : "") + (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
+}
+
+
+let inicio = 0;
+let timer = 0;
+let fim = 0;
+let iniciado = false;
+
+
 
 const Timer = ({ navigation, onEnd }) => {
     const interval = 100;
     const [info, setInfo] = useState({
         iniciado: false,
         timer: 0,
-        inicio: undefined,
-        fim: undefined,
-        showAtividade: false,
-        atividade: []
+        inicio: 0,
+        fim: 0,
+        showAtividade: false
     });
+    const [atividade, setAtividade] = useState([]);
+   
     const t = useRef();
+    const doTime = () => {
+        if (iniciado) {
+            timer = timer + Date.now() - inicio;
+            inicio = Date.now();
+            setInfo(info => ({ ...info, timer: timer }));
+        }
+        setTimeout(doTime, interval);
+    }
 
     useEffect(() => {
-        t.current = setInterval(() => {
-            if (info.iniciado) {
-                setInfo({ ...info, timer: Date.now() - info.inicio });
-            }
+        console.log(info, Date.now());
+        t.current = setTimeout(() => {
+            //if (info.iniciado) {
+            doTime();
+
+            //}
         }, interval);
         return () => {
-            clearInterval(t.current);
+            clearTimeout(t.current);
         }
-    }, [info]);
+    }, []);
 
     return (
         <View style={{ justifyContent: 'center' }} >
 
             <View style={{ alignItems: 'center' }}>
-                <Text h1>{Math.trunc(info.timer / 1000)}</Text>
+                <Text h1>{formatTime(Math.floor(info.timer / 1000))}</Text>
             </View>
             <Divider style={styles.divider} />
 
-            <View style={{flex:1, justifyContent:'center', flexDirection:'row', margin:25}}>
-                <View style={{  display: info.iniciado ? 'none' : 'flex' }}>
+            <View style={{ justifyContent: 'center', flexDirection: 'row', margin: 30 }}>
+                <View style={{ display: info.iniciado ? 'none' : 'flex' }}>
                     <Button onPress={() => {
-
-                        setInfo({ ...info, iniciado: true, inicio: Date.now(), fim: undefined });
+                        inicio = Date.now();
+                        fim = 0;
+                        iniciado = true;
+                        setInfo({ ...info, iniciado: true });
 
                     }}
-                        icon={{ name: 'play', type: 'font-awesome', color: '#FFF' }}
+                        icon={{ name: 'play-circle', type: 'font-awesome-5', color: '#FFF' }}
                         title="">Iniciar</Button>
                 </View>
-                <View style={{  display: info.iniciado ? 'flex' : 'none' }} >
+                <View style={{ display: info.iniciado ? 'flex' : 'none' }} >
                     <Button onPress={() => {
-                        setInfo({ ...info, iniciado: false });
-                        
+                        iniciado = false;
+                        fim = Date.now();
+                        setInfo({ ...info, iniciado: iniciado });
+
                     }}
-                        icon={{ name: 'pause', type: 'font-awesome', color: '#FFF' }}
+                        icon={{ name: 'pause-circle', type: 'font-awesome-5', color: '#FFF' }}
                         title="">Pausar</Button>
                 </View>
-                <View style={{  display: info.timer > 0 ? 'flex' : 'none' }}>
-                    <Button onLongPress={() => {
+                <View style={{ display: info.timer > 0 ? 'flex' : 'none' }}>
+                    <Button onPress={() => {
+                        iniciado = false;
                         setInfo({ ...info, iniciado: false, timer: 0, fim: Date.now() });
                         if (onEnd !== undefined) onEnd(info);
 
                     }}
-                        icon={{ name: 'stop', type: 'font-awesome', color: '#FFF' }}
+                        icon={{ name: 'stop-circle', type: 'font-awesome-5', color: '#FFF' }}
                         title="">Finalizar</Button>
                 </View>
             </View>
             <View style={{ marginBottom: 20 }}>
                 <Button onPress={() => {
-                    setInfo({ ...info, showAtividade: !info.showAtividade });
                     navigation.navigate('Atividade', {
-                        onEnd: (atividade) => {
-                            console.log("retorno atividade", atividade);
-                            setInfo({ ...info, atividade: [...info.atividade, atividade] });
+                        onEnd: (ativ) => {
+                            console.log("retorno atividade", ativ);
+                            setAtividade([...atividade, ativ]);
                         }
                     });
 
                 }}
-                    icon={{ name: 'plus-circle', type: 'font-awesome', color: '#FFF' }}
-                    title="Atividade">Inserir Atividade</Button>
+                    icon={{ name: 'plus-circle', type: 'font-awesome-5', color: '#FFF' }}
+                    title="Atividade">
+                    
+                </Button>
+                <Button onPress={() => {
+                
+
+
+                }}
+                    icon={{ name: 'save', type: 'font-awesome-5', color: '#FFF' }}
+                    title="Save"></Button>
+
             </View>
 
 
             <ScrollView >
                 <FlatList
-                    data={info.atividade}
-                    renderItem={({ item }) => (
-                        <ListItem bottomDivider>
+                    data={atividade}
+                    renderItem={({ item, index, key }) => (
+                        <ListItem.Swipeable
+                            rightContent={
+                                <Button
+                                    title="Delete"
+                                    icon={{ name: 'delete', color: 'white' }}
+                                    buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
+                                    onPress={() => {
+                                        setAtividade([...atividade.slice(0, index), ...atividade.slice(index + 1)]);
+
+                                    }}
+                                />
+                            }
+
+                            bottomDivider>
                             <ListItem.Content>
 
-                                <ListItem.Title >{item.nome}</ListItem.Title>
+                                <Avatar rounded title={index + 1} />
+                                <ListItem.Title >
+                                    {item.nome}
+                                </ListItem.Title>
                                 <ListItem.Subtitle>{item.descricao}</ListItem.Subtitle>
                             </ListItem.Content>
                             <ListItem.Chevron />
-                        </ListItem>
+                        </ListItem.Swipeable>
                     )}
-                    keyExtractor={(item, index) => index}
+                    keyExtractor={(item, index) => `${index}`}
                 />
 
 
